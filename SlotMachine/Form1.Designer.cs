91e10397f2addc4dfdc1d03b4a8f363976b64b0d
Olga Plotnitskaya <olga.plotnitskaya@gmail.com>
2021-05-25 18:07:40 +0300

    add project

---
+++ SlotMachine/Form1.Designer.cs
@@ -0,0 +1,250 @@
+﻿
+namespace SlotMachine
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.start = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.labelAttempts = new System.Windows.Forms.Label();
+            this.labelPoints = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Azure;
+            this.button1.Enabled = false;
+            this.button1.Font = new System.Drawing.Font("SketchFlow Print", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button1.Location = new System.Drawing.Point(12, 227);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(159, 211);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Azure;
+            this.button2.Enabled = false;
+            this.button2.Font = new System.Drawing.Font("SketchFlow Print", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(177, 227);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(159, 211);
+            this.button2.TabIndex = 1;
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Azure;
+            this.button3.Enabled = false;
+            this.button3.Font = new System.Drawing.Font("SketchFlow Print", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(342, 227);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(159, 211);
+            this.button3.TabIndex = 2;
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // start
+            // 
+            this.start.BackColor = System.Drawing.Color.LightGreen;
+            this.start.Font = new System.Drawing.Font("Rockwell Nova", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.start.ForeColor = System.Drawing.Color.DarkBlue;
+            this.start.Location = new System.Drawing.Point(342, 110);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(159, 111);
+            this.start.TabIndex = 3;
+            this.start.Text = "START";
+            this.start.UseVisualStyleBackColor = false;
+            this.start.Click += new System.EventHandler(this.start_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Rockwell Nova", 28.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.DarkBlue;
+            this.label1.Location = new System.Drawing.Point(26, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(449, 64);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "SLOT MACHINE";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Rockwell Nova", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.ForeColor = System.Drawing.Color.DarkBlue;
+            this.label2.Location = new System.Drawing.Point(12, 73);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(465, 34);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Get three idential symbols and WIN";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Rockwell Nova", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.ForeColor = System.Drawing.Color.DarkBlue;
+            this.label3.Location = new System.Drawing.Point(13, 143);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(122, 27);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "### - 100$";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Rockwell Nova", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.ForeColor = System.Drawing.Color.DarkBlue;
+            this.label4.Location = new System.Drawing.Point(12, 170);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(119, 27);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "777 - 500$";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Rockwell Nova", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.ForeColor = System.Drawing.Color.DarkBlue;
+            this.label5.Location = new System.Drawing.Point(12, 197);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(129, 27);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "$$$ - 1000$";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Rockwell Nova", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.ForeColor = System.Drawing.Color.DarkBlue;
+            this.label6.Location = new System.Drawing.Point(12, 116);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(115, 27);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "XXX - 50$";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Rockwell Nova", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label7.ForeColor = System.Drawing.Color.DarkBlue;
+            this.label7.Location = new System.Drawing.Point(237, 135);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(99, 27);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "attempts";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Rockwell Nova", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label8.ForeColor = System.Drawing.Color.DarkBlue;
+            this.label8.Location = new System.Drawing.Point(263, 183);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(73, 27);
+            this.label8.TabIndex = 13;
+            this.label8.Text = "points";
+            // 
+            // labelAttempts
+            // 
+            this.labelAttempts.AutoSize = true;
+            this.labelAttempts.BackColor = System.Drawing.Color.Azure;
+            this.labelAttempts.Font = new System.Drawing.Font("Rockwell Nova", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelAttempts.ForeColor = System.Drawing.Color.DarkBlue;
+            this.labelAttempts.Location = new System.Drawing.Point(172, 135);
+            this.labelAttempts.Name = "labelAttempts";
+            this.labelAttempts.Size = new System.Drawing.Size(0, 27);
+            this.labelAttempts.TabIndex = 14;
+            // 
+            // labelPoints
+            // 
+            this.labelPoints.AutoSize = true;
+            this.labelPoints.BackColor = System.Drawing.Color.Azure;
+            this.labelPoints.Font = new System.Drawing.Font("Rockwell Nova", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelPoints.ForeColor = System.Drawing.Color.DarkBlue;
+            this.labelPoints.Location = new System.Drawing.Point(172, 183);
+            this.labelPoints.Name = "labelPoints";
+            this.labelPoints.Size = new System.Drawing.Size(0, 27);
+            this.labelPoints.TabIndex = 15;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.ClientSize = new System.Drawing.Size(516, 450);
+            this.Controls.Add(this.labelPoints);
+            this.Controls.Add(this.labelAttempts);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.start);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button start;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label labelAttempts;
+        private System.Windows.Forms.Label labelPoints;
+    }
+}
+
---
+++ SlotMachine/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SlotMachine
+{
+    public partial class Form1 : Form
+    {
+        Random rnd = new Random();
+        int attempts = 0;
+        int points = 0;
+        int n;
+        char[] mas = new char[13] { 'x', '#', 'x', '7', '#', 'x', '7', 'x', '#', '$', 'x', '7', '#' };
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void start_Click(object sender, EventArgs e)
+        {
+            n = rnd.Next(1, 20);
+            int j = 0;
+            for (int i = 0; i < n; i++)
+            {
+                button1.Text = mas[j].ToString();
+                j++;
+                if(j == 13)
+                {
+                    j = 0;
+                }
+            }
+            n = rnd.Next(1, 20);
+            for (int i = 0; i < n; i++)
+            {
+                button2.Text = mas[j].ToString();
+                j++;
+                if (j == 13)
+                {
+                    j = 0;
+                }
+            }
+            n = rnd.Next(1, 20);
+            for (int i = 0; i < n; i++)
+            {
+                button3.Text = mas[j].ToString();
+                j++;
+                if (j == 13)
+                {
+                    j = 0;
+                }
+            }
+
+            if(button1.Text == button2.Text && button2.Text == button3.Text)
+            {
+                if(button1.Text == "x")
+                {
+                    points += 50;
+                }
+                else if(button1.Text == "#")
+                {
+                    points += 100;
+                }
+                else if(button1.Text == "7")
+                { 
+                    points += 500;
+                }
+                else if(button1.Text == "$")
+                {
+                    points += 1000;
+                }
+            }
+
+            attempts++;
+
+            labelAttempts.Text = attempts.ToString();
+            labelPoints.Text = points.ToString();
+        }
+    }
+}
